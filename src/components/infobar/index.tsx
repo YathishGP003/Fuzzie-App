aad234999773546e51f7af7527a2754ed5f9621c
Yathish G P <yathishgp1@gmail.com>
2024-10-23 21:02:08 +0530

    Dashboard created

---
+++ src/components/infobar/index.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { div } from 'framer-motion/client'
+import React from 'react'
+
+type Props = {}
+
+const InfoBar = (props: Props) => {
+    return (
+        <div>MenuOptions</div>
+    )
+}
+  
+export default InfoBar
