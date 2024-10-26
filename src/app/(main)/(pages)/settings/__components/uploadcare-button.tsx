2b4ebda05aaec0885ebf5ae2614460d1d77d744f
Yathish G P <yathishgp1@gmail.com>
2024-10-26 10:32:30 +0530

    Uploadcare Button error fixed

--- src/app/(main)/(pages)/settings/__components/uploadcare-button.tsx
+++ src/app/(main)/(pages)/settings/__components/uploadcare-button.tsx
@@ -1,57 +1,17 @@
 'use client' // is needed only if you’re using React Server Components
 import { FileUploaderRegular } from '@uploadcare/react-uploader/next';
 import '@uploadcare/react-uploader/core.css';
-import React, { useEffect, useRef } from 'react'
-import * as LR from '@uploadcare/blocks'
-import { useRouter } from 'next/navigation'
 
-type Props = {
-  onUpload: (e: string) => any
-}
-
-LR.registerBlocks(LR)
-
-const UploadCareButton = ({ onUpload }: Props) => {
-  const router = useRouter()
-  const ctxProviderRef = useRef<
-    typeof LR.UploadCtxProvider.prototype & LR.UploadCtxProvider
-  >(null)
-
-  useEffect(() => {
-    const handleUpload = async (e: any) => {
-      const file = await onUpload(e.detail.cdnUrl)
-      if(file) {
-        router.refresh()
-      }
-    }
-    ctxProviderRef.current?.addEventListener('file-upload-success', handleUpload)
-  }, [])
-
+function UploadCareButton() {
   return (
     <div>
-      <lr-config
-        ctx-name="my-uploader"
-        pubkey="13297b89aa490f01b980"
-      />
-
-      <lr-file-uploader-regular
-        ctx-name="my-uploader"
-        css-src={`https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.35.2/web/lr-file-uploader-regular.min.css`}
-      />
-
-      <lr-upload-ctx-provider
-        ctx-name="my-uploader"
-        ref={ctxProviderRef}
+      <FileUploaderRegular
+         sourceList="local, url, camera, dropbox"
+         classNameUploader="uc-light"
+         pubkey="13297b89aa490f01b980"
       />
     </div>
-    // <div>
-    //   <FileUploaderRegular
-    //     sourceList="local, url, camera, dropbox"
-    //     classNameUploader="uc-light"
-    //     pubkey="13297b89aa490f01b980"
-    //   />
-    // </div>
   );
 }
 
-export default UploadCareButton
+export default UploadCareButton;
